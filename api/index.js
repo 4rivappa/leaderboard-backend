f508addd399cda8e7ea45fff4795c456ef035230
4rivappa <4rivappa@proton.me>
2023-10-02 19:46:33 +0530

    hell

---
+++ api/index.js
@@ -0,0 +1,29 @@
+const express = require("express")
+const profileRouter = require("../routes/profile");
+const connectToMongoDB = require("../controllers/dbConnection");
+const cors = require("cors");
+
+const app = express()
+require('dotenv').config();
+
+app.use(cors());
+
+async function connectToDatabase(){
+    try {
+        await connectToMongoDB(process.env.DB_URI);
+    } catch (error) {
+        console.error(error);
+        process.exit(1);
+    }
+}
+connectToDatabase()
+
+app.get('/favicon.ico', (req, res) => res.status(204));
+
+app.get('/', (req, res) => {
+    res.send("4rivappa backend")
+})
+
+app.use("/profile", profileRouter);
+
+app.listen(4068)
